b8202b46230126f3d52fb22d57e53983def4e005
Ivan Skovpin <ivanskovpin@gmail.com>
2021-01-06 22:38:35 +0700

    Initial Commit

---
+++ Memorize (Stanford)/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize (Stanford)
+//
+//  Created by Иван С on 06.01.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Memorize (Stanford)/Memorize__Stanford_App.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize__Stanford_App.swift
+//  Memorize (Stanford)
+//
+//  Created by Иван С on 06.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct Memorize__Stanford_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
