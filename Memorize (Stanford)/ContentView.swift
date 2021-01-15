53c29e6385a6b6fb1f82ae078b835602d64218da
Ivan Skovpin <ivanskovpin@gmail.com>
2021-01-15 21:45:40 +0700

    + pretty card view

--- Memorize (Stanford)/ContentView.swift
+++ Memorize (Stanford)/ContentView.swift
@@ -9,42 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var viewModel: EmojiMemoryGame
-    
+
     var body: some View {
         HStack {
+            let font = viewModel.cards.count > 4 ? Font.title : Font.largeTitle
             ForEach(viewModel.cards.shuffled()) {
                 card in
-                CardView(card: card).onTapGesture {
+                CardView(card: card, font: font).onTapGesture {
                     viewModel.choose(card: card)
                 }
             }
-        }
-        .foregroundColor(Color.orange)
-        .padding()
+        }.foregroundColor(Color.orange)
+                .padding()
     }
 }
 
 struct CardView: View {
     var card: MemoryGame<String>.Card
-    
+    var font: Font = Font.largeTitle
+
     var body: some View {
         if card.isFaceUp {
             ZStack {
                 RoundedRectangle(cornerRadius: 10).fill(Color.white)
                 RoundedRectangle(cornerRadius: 10).stroke(lineWidth: 3)
-                Text(card.content).font(Font.largeTitle)
-            }
+                Text(card.content).font(font)
+            }.aspectRatio(2 / 3, contentMode: .fit)
         } else {
             RoundedRectangle(cornerRadius: 10).fill(Color.orange)
+                    .aspectRatio(2 / 3, contentMode: .fit)
         }
     }
 }
-
-
-
-
-
-
 
 
 struct ContentView_Previews: PreviewProvider {
